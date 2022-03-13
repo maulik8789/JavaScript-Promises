d06cac07cb23f9e2fdf8cbf610dd969524640bd7
maulikvaghela <maulik.vagh@gmail.com>
2022-03-13 16:55:04 -0400

    async awaited

--- planet.js
+++ planet.js
@@ -3,8 +3,12 @@ let favNum = document.querySelector("#num")
 let radVal = document.getElementsByName("radio");
 let result = document.getElementById("fact")
 let btnCard = document.getElementById("btnCard")
+let cardAwait = document.getElementById("cardAwait")
+let btnCardAwait = document.getElementById("btnCardAwait")
 let deckId = ""
 let cardMargin = 5
+let newCount = 1
+
 
 form.addEventListener("submit", (evt) => {
   evt.preventDefault()
@@ -81,4 +85,45 @@ formCard.addEventListener("click", (evt) => {
     });
   }
     
+})
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//WITH ASYNC & AWAIT
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+let deck = {
+  async init(){
+    let res = await axios.get (`http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+    this.currDeckId = res.data.deck_id;
+  },
+  async shuffleCard(){
+    let res = await axios.get(`http://deckofcardsapi.com/api/deck/${this.currDeckId}/shuffle/?count=1`)
+  },
+  async getCard(){
+    let res = await axios.get(`http://deckofcardsapi.com/api/deck/${this.currDeckId}/draw/?count=1`)
+    
+    
+    let image = document.createElement("img")
+    image.src = res.data.cards[0].image
+    console.log(image.src)
+    document.body.appendChild(image)
+    image.style.position = "absolute"
+    image.style.marginLeft = `${cardMargin}px`;
+    cardMargin += 25  
+    newCount++;
+  }
+
+}
+
+deck.init()
+cardMargin = 0
+
+cardAwait.addEventListener("submit", (e) =>{
+  e.preventDefault();
+  if (newCount <= 52){
+  deck.shuffleCard()
+  deck.getCard()
+  }
 })
